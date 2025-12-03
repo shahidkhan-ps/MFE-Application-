a8a191072330723b4665f653431ef030f9a8208e
shahidkhan-ps <shahid.khan@platformscience.com>
2025-12-03 14:22:07 +0530

    setup of 2 remote application-2 with federation setup

--- MFE-Remote-1/vite.config.js
+++ MFE-Remote-1/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { federation } from '@module-federation/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name:"todo",
+      filename:"remoteEntry.js",
+      exposes:{
+        "./Todo":"./src/App.jsx",
+      },
+      shared:["react","react-dom"]
+    })  
+  ],
+  build:{
+    target:"esnext"
+  }
 })
